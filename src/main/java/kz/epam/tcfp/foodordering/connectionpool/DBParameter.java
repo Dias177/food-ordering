84b3ad3e0925a4428a49d7a55bff76605b1b958b
Dias177 <zhassanbay@gmail.com>
2021-01-06 00:41:55 +0600

    refactor: remove unnecessary db properties

--- src/main/java/kz/epam/tcfp/foodordering/connectionpool/DBParameter.java
+++ src/main/java/kz/epam/tcfp/foodordering/connectionpool/DBParameter.java
@@ -5,13 +5,8 @@ public final class DBParameter {
     private DBParameter() {}
 
     public static final String DB_DRIVER = "db.driver";
+    public static final String DB_URL = "db.url";
     public static final String USER = "user";
     public static final String PASSWORD = "password";
     public static final String POOL_SIZE = "poolsize";
-    public static final String DB_URL = "db.url";
-    public static final String USE_UNICODE = "useUnicode";
-    public static final String ENCODING = "encoding";
-    public static final String USE_JDBC_COMPLIANT_TIMEZONE_SHIFT = "useJDBCCompliantTimezoneShift";
-    public static final String USE_LEGACY_DATETIME_CODE = "useLegacyDatetimeCode";
-    public static final String SERVER_TIMEZONE = "serverTimezone";
 }
