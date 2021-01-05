59794d97cfdd44b2d256bddde1898a4b5a20dc12
Dias177 <zhassanbay@gmail.com>
2021-01-05 21:09:32 +0600

    feat: add db resource manager

---
+++ src/main/java/kz/epam/tcfp/foodordering/connectionpool/DBResourceManager.java
@@ -0,0 +1,17 @@
+package kz.epam.tcfp.foodordering.connectionpool;
+
+import java.util.ResourceBundle;
+
+public class DBResourceManager {
+
+    private static final DBResourceManager instance = new DBResourceManager();
+    private ResourceBundle bundle = ResourceBundle.getBundle("database");
+
+    public static DBResourceManager getInstance() {
+        return instance;
+    }
+
+    public String getValue(String key) {
+        return bundle.getString(key);
+    }
+}
