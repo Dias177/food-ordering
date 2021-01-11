8da7ff7521282136d8b161af2e888e624067f7f6
Dias177 <zhassanbay@gmail.com>
2021-01-11 16:39:16 +0600

    init: start service

---
+++ src/main/java/kz/epam/tcfp/foodordering/controller/FoodOrderingController.java
@@ -0,0 +1,31 @@
+package kz.epam.tcfp.foodordering.controller;
+
+import kz.epam.tcfp.foodordering.connectionpool.ConnectionPool;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FoodOrderingController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void destroy() {
+        ConnectionPool.getInstance().destroyPool();
+    }
+}
---
+++ src/main/java/kz/epam/tcfp/foodordering/service/Service.java
@@ -0,0 +1,7 @@
+package kz.epam.tcfp.foodordering.service;
+
+import javax.servlet.http.HttpServletRequest;;
+
+public interface Service {
+    String execute(HttpServletRequest req);
+}
