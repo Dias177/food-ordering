c5f6c66b0947a946b8d2ddc9eeb7cd35f8b092b3
Dias177 <zhassanbay@gmail.com>
2021-01-28 18:29:31 +0600

    refactor: add inserting food description

--- Insert.sql
+++ Insert.sql
@@ -11,19 +11,19 @@ VALUES (2, 'user2@test.com', '96dee1bb26cf7cd9167a4882dc494b8c', 'User', 'Userov
 
 INSERT INTO food_category (name) VALUES ('Japanese');
 INSERT INTO food_category (name) VALUES ('European');
-INSERT INTO food_category (name) VALUES ('Russian');
+INSERT INTO food_category (name) VALUES ('Русская');
 INSERT INTO food_category (name) VALUES ('Asian');
-INSERT INTO food_category (name) VALUES ('Turkish');
+INSERT INTO food_category (name) VALUES ('Турецкая');
 INSERT INTO food_category (name) VALUES ('Italian');
 
-INSERT INTO food (food_category_id, name, price)
-VALUES (1, 'Sushi', 3000);
-INSERT INTO food (food_category_id, name, price)
-VALUES (5, 'Doner', 1000);
-INSERT INTO food (food_category_id, name, price)
-VALUES (6, 'Pasta', 2000);
-INSERT INTO food (food_category_id, name, price)
-VALUES (3, 'Borsh', 1000);
+INSERT INTO food (food_category_id, name, description, price)
+VALUES (1, 'Sushi', 'Some text', 3000);
+INSERT INTO food (food_category_id, name, description, price)
+VALUES (5, 'Донер', 'Некоторый текст',1000);
+INSERT INTO food (food_category_id, name, description, price)
+VALUES (6, 'Pasta', 'Some text', 2000);
+INSERT INTO food (food_category_id, name, description, price)
+VALUES (3, 'Борщ', 'Некоторый текст', 1000);
 
 INSERT INTO order_status (name) VALUES ('In processing');
 INSERT INTO order_status (name) VALUES ('Preparing');
