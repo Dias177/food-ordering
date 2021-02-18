3aaf307c1d3fc2c23207c04ed23228c0afa2c1aa
Dias177 <zhassanbay@gmail.com>
2021-02-19 02:07:35 +0600

    feat: add role dao methods

--- src/main/java/kz/epam/tcfp/foodordering/dao/RoleDao.java
+++ src/main/java/kz/epam/tcfp/foodordering/dao/RoleDao.java
@@ -16,6 +16,28 @@ public class RoleDao extends AbstractDao<Long, Role> {
     private static final String SQL_SELECT_ROLE_BY_ID = "SELECT * FROM role WHERE id = ?";
     private static final String SQL_SELECT_ALL_ROLES = "SELECT * FROM role ORDER BY id";
     private static final String SQL_DELETE_ROLE_BY_ID = "DELETE FROM role WHERE id = ?";
+    private static final String SQL_UPDATE_ROLE = "UPDATE role SET name = ? WHERE id = ?";
+    private static final String SQL_CREATE_ROLE = "INSERT INTO role (name) VALUES (?)";
+    private static final String SQL_SELECT_ROLE_BY_NAME = "SELECT * FROM role WHERE name = ?";
+
+    public Role findRoleByName(String name) throws DaoException {
+        Role role = new Role();
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_SELECT_ROLE_BY_NAME);
+            statement.setString(1, name);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                role.setId(resultSet.getLong(COLUMN_LABEL_ID));
+                role.setName(resultSet.getString(COLUMN_LABEL_NAME));
+            }
+        } catch (SQLException e) {
+            throw new DaoException("Error in finding role by name", e);
+        } finally {
+            close(statement);
+        }
+        return role;
+    }
 
     @Override
     public List<Role> findAll() throws DaoException {
@@ -89,14 +111,40 @@ public class RoleDao extends AbstractDao<Long, Role> {
         }
         return rows > 0;
     }
-    //TODO: Implement add role
+
     @Override
     public boolean create(Role role) throws DaoException {
-        return false;
+        Role r = findRoleByName(role.getName());
+        if (r.getName() != null && r.getId() != 0) {
+            return false;
+        }
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_CREATE_ROLE);
+            statement.setString(1, role.getName());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DaoException("Error in creating role", e);
+        } finally {
+            close(statement);
+        }
+        return true;
     }
-    //TODO: Implement edit role
+
     @Override
     public int update(Role role) throws DaoException {
-        return 0;
+        int rows;
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_UPDATE_ROLE);
+            statement.setString(1, role.getName());
+            statement.setLong(2, role.getId());
+            rows = statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DaoException("Error in updating role", e);
+        } finally {
+            close(statement);
+        }
+        return rows;
     }
 }
